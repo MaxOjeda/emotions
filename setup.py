8fc361e96c7f835cdeee033cf0a5242ea4b35d34
MaxOjeda <38363450+MaxOjeda@users.noreply.github.com>
2021-10-05 17:50:07 -0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-setup(
-    name='emociones',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Emotions Analysis Library',
-    author='@dlefimil and @MaxOjeda',
-)
