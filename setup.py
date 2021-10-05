40c90e624e0e5dd2306a5722f630f71d1204b04f
MaxOjeda <38363450+MaxOjeda@users.noreply.github.com>
2021-10-05 17:50:23 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name='emotions',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Emotions Analysis Library',
+    author='@dlefimil and @MaxOjeda',
+)
